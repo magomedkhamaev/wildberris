c0b054c4613f65bbe7d2aec03de2fdf993747e61
magomedkhamaev <125343031+magomedkhamaev@users.noreply.github.com>
2023-03-02 16:13:36 +0300

    ch

--- js/index.js
+++ js/index.js
@@ -34,7 +34,7 @@
             const array = category ? data.filter((item) => item[category] === value) : data
             
             localStorage.setItem('goods', JSON.stringify(array));
-           if(window.location.pathname !== "wildberris/goods.html") {
+           if(window.location.pathname !== "goods.html") {
             window.location.href = 'goods.html'
            } else {
             renderGoods(array)
@@ -52,7 +52,7 @@
         })
     })
     
-   if(localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+   if(localStorage.getItem('goods') && window.location.pathname === "goods.html") {
     renderGoods(JSON.parse(localStorage.getItem('goods')))
    }
     // const goods = JSON.parse(localStorage.getItem('goods'))
